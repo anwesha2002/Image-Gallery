d24822283db83eee888956af5b99d1407def05e4
anwesha <ANWESHA@DESKTOP-ALJFO4P.com>
2023-10-30 13:00:56 -0400

    refac: interface extended

--- src/data/model/gallery.ts
+++ src/data/model/gallery.ts
@@ -1,28 +1,35 @@
-export interface Gallery{
-    user : {name : string},
-    urls : {thumb : string},
+interface user {
+    name : string
+    username : string,
+    social : {
+        instagram_username : string,
+        twitter_username : string,
+        portfolio_url : string
+    }
+    profile_image : {medium : string},
+    links : {
+        photos : string
+    }
+}
+
+interface links{
+    download : string,
+    self : string
+}
+
+interface urls {
+    thumb : string,
+    raw : string
+}
+
+export interface Gallery extends user, urls{
     likes : number,
     id : string,
     alt_description : string,
 }
 
-export interface PopUpModalProps {
-    urls : {raw : string},
-    user : {
-        username : string,
-        name : string,
-        social : {
-            instagram_username : string,
-            twitter_username : string,
-            portfolio_url : string
-        }
-        profile_image : {medium : string},
-        links : {
-            photos : string
-        }
-    }
+export interface PopUpModalProps extends Gallery, links {
     views : number,
     likes : number,
     id : string,
-    links : { download : string, self : string}
 }
